a6b396db55e1a9c4f75027cf39a33772338a1dac
Orion Gael <orion.gael@gmail.com>
2020-03-20 02:22:44 -0700

    updated final project

--- des157-wi20/final/script.js
+++ des157-wi20/final/script.js
@@ -1,112 +1,36 @@
-(function(){
+window.onload=function(){
     "use strict";
 
-    var canvas, ctx, flag = false,
-        prevX = 0,
-        currX = 0,
-        prevY = 0,
-        currY = 0,
-        dot_flag = false;
+    const can = document.querySelector("canvas");
+    const controls = document.getElementsByClassName("control");
+    const colors = document.getElementsByClassName("color");
+    let controlIndex = 0;
 
-    var col = "black",
-        pix = 4;
-
-    function init() {
-        canvas = document.getElementById('can');
-        ctx = canvas.getContext("2d");
-        w = canvas.width;
-        h = canvas.height;
-
-        canvas.addEventListener("mousemove", function(e){
-            findxy('move', e)
-        }, false);
-        canvas.addEventListener("mousedown", function(e){
-            findxy('down', e)
-        }, false);
-        canvas.addEventListener("mouseup", function(e){
-            findxy('up', e)
-        }, false);
-        canvas.addEventListener("mouseout", function(e){
-            findxy('out', e)
-        }, false);
+    function deactivate(c){
+        c.getAttribute("src").replace("1", "0");
     }
 
-    function color(obj) {
-        switch (obj.id) {
-            case "black":
-                col = "black";
-                break;
-            case "red":
-                col = "red";
-                break;
-            case "blue":
-                col = "blue";
-                break;
-            case "yellow":
-                col = "yellow";
-                break;
-            case "white":
-                col = "white";
-                break;
+    function activate(c){
+        c.getAttribute("src").replace("0", "1");
+    }
+
+    controls.forEach(this.addEventListener("click", function(e){
+        if(controlIndex != controls.indexOf(e.target)){
+            controls.forEach(deactivate(e.target));
+            activate(e.target);
         }
-        if (col == "white") y = 14;
-        else y = 2;
 
-    }
+    }));
+    
 
-    function draw() {
-        ctx.beginPath();
-        ctx.moveTo(prevX, prevY);
-        ctx.lineTo(currX, currY);
-        ctx.strokeStyle = col;
-        ctx.lineWidth = y;
-        ctx.stroke();
-        ctx.closePath();
-    }
-
-    function erase() {
-        var m = confirm("Want to clear");
-        if (m) {
-            ctx.clearRect(0, 0, w, h);
-            document.getElementById("canvasimg").style.display = "none";
+    controls[1].addEventListener("click", function(){
+        if(controlIndex != 1){
+            controls[1].src = "images/lineS1.png";
         }
-    }
+    });
 
-    function save() {
-        document.getElementById("canvasimg").style.border = "2px solid";
-        var dataURL = canvas.toDataURL();
-        document.getElementById("canvasimg").src = dataURL;
-        document.getElementById("canvasimg").style.display = "inline";
-    }
-
-    function findxy(res, e) {
-        if (res == 'down') {
-            prevX = currX;
-            prevY = currY;
-            currX = e.clientX - canvas.offsetLeft;
-            currY = e.clientY - canvas.offsetTop;
-
-            flag = true;
-            dot_flag = true;
-            if (dot_flag) {
-                ctx.beginPath();
-                ctx.fillStyle = col;
-                ctx.fillRect(currX, currY, 2, 2);
-                ctx.closePath();
-                dot_flag = false;
-            }
-        }
-        if (res == 'up' || res == "out") {
-            flag = false;
-        }
-        if (res == 'move') {
-            if (flag) {
-                prevX = currX;
-                prevY = currY;
-                currX = e.clientX - canvas.offsetLeft;
-                currY = e.clientY - canvas.offsetTop;
-                draw();
-            }
-        }
-    }
-}())
+    var canvas = new fabric.Canvas('can');
+    canvas.isDrawingMode = true;
+    canvas.freeDrawingBrush.width = 5;
+    canvas.freeDrawingBrush.color = "#000";
+}
